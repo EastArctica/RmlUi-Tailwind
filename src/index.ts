c2b3c1bbf0c5ca020e449262a40580235446794f
Jack Manning <eastarctica@gmail.com>
2026-04-02 19:11:37 -0400

    Add real module entrypoints & types

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { buildRcss } from './build.js';
+export { loadConfig, type RmluiTailwindConfig } from './config.js';
+export { generateSupportData, writeSupportData } from './generate-support.js';
+export {
+    transformCss,
+    writeTransformedCss,
+    type TransformResult,
+} from './transform-css.js';
